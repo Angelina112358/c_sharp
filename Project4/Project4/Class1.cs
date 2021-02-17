3c24875ad23cb530a0f082a04aceff3b6173186a
Angelina112358 <79206480+Angelina112358@users.noreply.github.com>
2021-02-17 23:42:21 +0300

    Add files via upload

---
+++ Project4/Project4/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4{
+    class Class0
+    {
+       public int a;
+       public static Class0 c;
+
+    }
+    class Class1:Class0
+    {
+        public static void Main(string[] args) {
+            Class0 c = new Class0(); 
+            int b;
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(b);
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+
+        }
+    }
+}
