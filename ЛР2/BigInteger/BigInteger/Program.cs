d703e67e2dbb4630d07bd9827162e55fa969372f
Angelina112358 <79206480+Angelina112358@users.noreply.github.com>
2021-03-13 02:51:25 +0300

    Add files via upload

---
+++ ЛР2/BigInteger/BigInteger/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Numerics;
+
+namespace Biginteger
+{
+    class Program
+    {
+        static BigInteger Multiply(BigInteger number1, BigInteger number2)
+        {
+            BigInteger result = 1;
+            for(BigInteger i = number1; i <= number2; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a: ");
+            BigInteger a;
+            while (true)
+            {
+                string number1 = Console.ReadLine();
+                bool result1 = BigInteger.TryParse(number1, out a);
+                if (!result1||a<=0) Console.WriteLine("Введите число а: ");
+                else break;
+            }
+            BigInteger b;
+            Console.WriteLine("Введите число b: ");
+            while (true)
+            {
+                string number2 = Console.ReadLine();
+                bool result2 = BigInteger.TryParse(number2, out b);
+                if (!result2||b<=0) Console.WriteLine("Введите число b: ");
+                else break; 
+            }
+            if(a>b)
+            {
+                BigInteger c = a;
+                a = b;
+                b = c;
+            }
+            Console.WriteLine("Произведение: " + Multiply(a, b));
+            BigInteger result = Multiply(a, b);
+            BigInteger degree=0;
+            while(result % 2 == 0)
+            {
+                result = result / 2;
+                degree++;
+            }
+            Console.WriteLine("Максимальная степень двойки: " + degree);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ЛР2/DateTime/DateTime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.ToString("G"));
+            Console.WriteLine(date.ToString("F"));
+            string allTime = date.ToString("yyyyMMddHHmmss");
+            Console.WriteLine(allTime);
+            int[] digits = new int[allTime.Length];
+            for(int i = 0; i <= 9; i++)
+            {
+                digits[i] = 0;
+            }
+            for(int i = 0; i < allTime.Length; i++)
+            {
+                if(allTime[i]>='0'&&allTime[i]<='9')
+                { 
+                    digits[allTime[i] - 48] ++;   
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Количество {i}: " + digits[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
