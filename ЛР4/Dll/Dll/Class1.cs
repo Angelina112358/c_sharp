028c6f6c3a5f24745d695cbd7d4d211c37126004
Angelina112358 <79206480+Angelina112358@users.noreply.github.com>
2021-04-15 14:50:08 +0300

    Add files via upload

---
+++ ЛР4/Dll/Dll/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dll
+{
+    public class Calculator
+    {
+        static public void Actions()
+        {
+            double a, b, c;
+            string number;
+            char sign;
+            do
+            {
+                Console.WriteLine("Введите первое число: ");
+                number = Console.ReadLine();
+                Console.Clear();
+            } while (!double.TryParse(number, out a));
+            do
+            {
+                Console.WriteLine("Введите второе число: ");
+                number = Console.ReadLine();
+                Console.Clear();
+            } while (!double.TryParse(number, out b));
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Введите знак (+, -, *, /)");
+                number = Console.ReadLine();
+                Console.Clear();
+            } while (!char.TryParse(number, out sign) || (sign != '+' && sign != '-' && sign != '*' && sign != '/'));
+            switch (sign)
+            {
+                case '+':
+                c = a + b;
+                Console.WriteLine("{0}" + sign + "{1} = {2}", a, b, c);
+                    break;
+                case '-':
+                c = a - b;
+                Console.WriteLine("{0}" + sign + "{1} = {2}", a, b, c);
+                    break;
+                case '*':
+                c = a * b;
+                Console.WriteLine("{0}" + sign + "{1} = {2}", a, b, c);
+                    break;
+                case '/':
+                    if (b == 0) Console.WriteLine("/0 нельзя");
+                    else
+                    {
+                        c = a / b;
+                        Console.WriteLine("{0}" + sign + "{1} = {2}", a, b, c);
+                    }
+                    break;
+            } 
+        }
+    }
+    public class Tetrahedron 
+    {
+        static public void Volume()
+        {
+            double a;
+            string number;
+            double volume;
+            do
+            {
+                Console.WriteLine("Введите длину стороны: ");
+                number = Console.ReadLine();
+                Console.Clear();
+            } while (!double.TryParse(number, out a)||a <= 0);
+            volume = (Math.Pow(a, 3)*Math.Sqrt(2)) / 12;
+            Console.WriteLine(volume);
+        }
+    }
+}
---
+++ ЛР4/forDll/forDll/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dll;
+
+namespace forDll
+{
+    class Class1
+    {
+        static public void Main(String []args)
+        {
+            char contin = ' ';
+            while (contin!='N')
+            {
+                Console.Clear();
+                Dll.Tetrahedron.Volume();
+                Console.WriteLine("Если хотите выйти, нажмите \"N\"." +
+                    "Если хотите продолжить, нажмите любую клавишу.");
+                contin = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
